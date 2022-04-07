30924e10d41b4c2571e0bd3a4863bf5551d5666d
Juha Ritakoski <juha.ritakoski@hotmail.com>
2022-04-07 22:25:41 +0300

    add missing semicolons

--- functions/initGuild.js
+++ functions/initGuild.js
@@ -3,18 +3,18 @@ const guildInfo = require("../guildInfo.json")
 
 module.exports = (client, guild) => {
     if (!guildInfo[guild.id]) {
-        guildInfo[guild.id] = {}
+        guildInfo[guild.id] = {};
     }
     if (!guildInfo[guild.id]["roles"]) {
-        guildInfo[guild.id]["roles"] = {}
+        guildInfo[guild.id]["roles"] = {};
     }
     if (!guildInfo[guild.id]["signups"]) {
-        guildInfo[guild.id]["signups"] = {}
+        guildInfo[guild.id]["signups"] = {};
     }
     if (!guildInfo[guild.id]["description"]){
-        guildInfo[guild.id]["description"] = "Default description"
+        guildInfo[guild.id]["description"] = "Default description";
     }
     fs.writeFile("./guildInfo.json", JSON.stringify(guildInfo, null, 4), async (error) => {
-        if (error) console.error(error)
+        if (error) console.error(error);
     })
 }
