9816a9df47f24c268deee0aa40b18fd5ff9bbd33
Juha Ritakoski <juha.ritakoski@hotmail.com>
2022-04-15 19:51:38 +0300

    fix channel type check for archive command

--- commands/archive.js
+++ commands/archive.js
@@ -8,16 +8,25 @@ module.exports = {
         .setDescription("Archives schedules into #archive channel.")
     ],
     async execute(interaction, client) {
+        try {
+            const sent = await interaction.reply(`Archiving messages.`);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setTimeout(async () => {
+                await interaction.deleteReply();
+            }, 10000)
+        }
         let archiveChannel = null;
         for (const [id, channel] of interaction.guild.channels.cache) {
-            if (channel.name == "archive" && channel.type == "text") {
+            if (channel.name == "archive" && channel.type == "GUILD_TEXT") {
                 archiveChannel = channel;
                 break;
             }
         }
         if (!archiveChannel) {
             try {
-                archiveChannel = await interaction.guild.createChannel('archive', { type: "text" });
+                archiveChannel = await interaction.guild.channels.create('archive', { type: "text" });
             } catch (error) { console.error(error) };
         }
         await interaction.channel.messages.fetch();
@@ -35,16 +44,6 @@ module.exports = {
                     console.error(error);
                 }
             }
-        }
-
-        try {
-            const sent = await interaction.reply(`Messages archived.`);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setTimeout(async () => {
-                await interaction.deleteReply();
-            }, 10000)
         }
     }
 }
