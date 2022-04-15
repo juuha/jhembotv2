72e7389c4ffa020563dddd640b843e105eb5877b
Juha Ritakoski <juha.ritakoski@hotmail.com>
2022-04-15 15:08:23 +0300

    finish addrole command

--- commands/addRole.js
+++ commands/addRole.js
@@ -1,14 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const guildInfo = require('../guildInfo.json');
 const fs = require("fs");
-const { rawListeners } = require('process');
 
 module.exports = {
     data: [new SlashCommandBuilder()
         .setName("addrole")
         .setDescription("Starts a role addition process!")
         .addStringOption(option =>
-            option.setName("new_role")
+            option.setName("role_name")
                 .setDescription("Name of the new role.")
                 .setRequired(true))
         .addStringOption(option =>
@@ -24,7 +23,7 @@ module.exports = {
                 let emoji = guildInfo[guildId]["roles"][newRole];
                 let custom_emoji = client.emojis.cache.find(emoji => emoji.name == guildInfo[guildId]["roles"][newRole]);
                 if (custom_emoji) emoji = custom_emoji;
-                const sent = await interaction.reply(`Role ${newRole} already exists with emoji ${emoji}, choose another name for the role or remove old role first.`);
+                const sent = await interaction.reply(`Role ${newRole} already exists with emoji ${emoji}, choose another name or remove old role first.`);
             } catch (error) {
                 console.error(error);
             } finally {
@@ -43,21 +42,43 @@ module.exports = {
         if (match) {
             customEmoji = client.emojis.cache.find(emoji => emoji.id == match[3])
         }
-
-        if (customEmoji) {
-            // TODO custom emoji found
-            console.log('custom', customEmoji)
-        } else {
-            let regex = /\p{Emoji_Presentation}/gu;
-            let emo = givenEmoji.match(regex);
-            if (emo) {
-                // TODO found regular emoji
-                console.log("regular", emo[0])
+        let emoj = null;
+        if (!customEmoji) {
+            // Hopefully all emojis.
+            let regex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/;
+            emoj = givenEmoji.match(regex);
+            if (!emoj) {
+                try {
+                    const sent = await interaction.reply(`No emoji found in \"${givenEmoji}\".`);
+                } catch (error) {
+                    console.error(error);
+                } finally {
+                    setTimeout(async () => {
+                        await interaction.deleteReply();
+                    }, 10000)
+                }
+                return;
             }
-            // TODO no emoji found
-            else console.log("no emoji")
         }
 
-        await interaction.reply(`yo`);
+        if (customEmoji) {
+            guildInfo[guildId]["roles"][newRole] = customEmoji.name;
+        } else {
+            guildInfo[guildId]["roles"][newRole] = emoj[1];
+        }
+        fs.writeFile("./guildInfo.json", JSON.stringify(guildInfo, null, 4), async (error) => {
+            if (error) console.error(error);
+        })
+
+        const emoji = customEmoji ? customEmoji : emoj[1];
+        try {
+            const sent = await interaction.reply(`Role ${newRole} added with emoji ${emoji}.`);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setTimeout(async () => {
+                await interaction.deleteReply();
+            }, 10000)
+        }
     }
 }
