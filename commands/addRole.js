7928e91fe5dff1c426e6b8d8c38053a8797df6f0
Juha Ritakoski <juha.ritakoski@hotmail.com>
2022-04-15 13:31:38 +0300

    Add emoji resolver

--- commands/addRole.js
+++ commands/addRole.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const guildInfo = require('../guildInfo.json');
 const fs = require("fs");
+const { rawListeners } = require('process');
 
 module.exports = {
     data: [new SlashCommandBuilder()
@@ -8,12 +9,16 @@ module.exports = {
         .setDescription("Starts a role addition process!")
         .addStringOption(option =>
             option.setName("new_role")
-                .setDescription("role name to add")
+                .setDescription("Name of the new role.")
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName("role_emoji")
+                .setDescription("Emoji used for the role.")
                 .setRequired(true))
     ],
     async execute(interaction, client) {
         const guildId = interaction.guild.id;
-        let newRole = interaction.options.getString("new_role");
+        let newRole = interaction.options.getString("role_name");
         if (guildInfo[guildId]["roles"][newRole]) {
             try {
                 let emoji = guildInfo[guildId]["roles"][newRole];
@@ -25,9 +30,32 @@ module.exports = {
             } finally {
                 setTimeout(async () => {
                     await interaction.deleteReply();
-                }, 2000)                
+                }, 10000)
             }
             return;
+        }
+
+        const givenEmoji = interaction.options.getString("role_emoji")
+        const emojiStringSplitter = /<(a?):(\w+):(\d+)>/;
+        let match = givenEmoji.match(emojiStringSplitter);
+        var customEmoji = null;
+
+        if (match) {
+            customEmoji = client.emojis.cache.find(emoji => emoji.id == match[3])
+        }
+
+        if (customEmoji) {
+            // TODO custom emoji found
+            console.log('custom', customEmoji)
+        } else {
+            let regex = /\p{Emoji_Presentation}/gu;
+            let emo = givenEmoji.match(regex);
+            if (emo) {
+                // TODO found regular emoji
+                console.log("regular", emo[0])
+            }
+            // TODO no emoji found
+            else console.log("no emoji")
         }
 
         await interaction.reply(`yo`);
