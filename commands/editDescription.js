64d2ec5d8b07e94de9888da52c1983123f462bfe
Juha Ritakoski <juha.ritakoski@hotmail.com>
2022-04-16 12:13:56 +0300

    finish editDescription command

--- commands/editDescription.js
+++ commands/editDescription.js
@@ -12,6 +12,23 @@ module.exports = {
                 .setRequired(true))
     ],
     async execute(interaction, client) {
-        
+        const guildId = interaction.guild.id;
+        let newDescription = interaction.options.getString("new_description");
+
+        guildInfo[guildId]["description"] = newDescription;
+
+        fs.writeFile("./guildInfo.json", JSON.stringify(guildInfo, null, 4), async (error) => {
+            if (error) console.error(error);
+        });
+
+        try {
+            const sent = await interaction.reply(`Description set.`);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setTimeout(async () => {
+                await interaction.deleteReply();
+            }, 10000)
+        }
     }
 }
